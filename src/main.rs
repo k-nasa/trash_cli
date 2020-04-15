309e96aec291f9e714b119659246fac361a7f7fe
Nils <nils@nilsand.re>
2020-04-15 17:36:32 +0100

    Rewrite crate to use trash

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-extern crate trash_cli;
-use trash_cli::run;
+// TODO: remove this line when the clap version used has this commit:
+// https://github.com/clap-rs/clap/commit/56d182d98bb51401ae99d071a4fad772351dc1b4
+use clap::{crate_authors, crate_description, crate_name, crate_version};
+
+use clap::{app_from_crate, AppSettings, Arg, SubCommand};
 
 fn main() {
-    run()
+    let matches = app_from_crate!()
+        .setting(AppSettings::DeriveDisplayOrder)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(SubCommand::with_name("clean").about("Clean trash directory"))
+        .subcommand(
+            SubCommand::with_name("rm").about("Trash file remove").arg(
+                Arg::with_name("path")
+                    .required(true)
+                    .multiple(true)
+                    .help("Remove file path"),
+            ),
+        )
+        .get_matches();
+    match matches.subcommand() {
+        ("clean", Some(_)) => {
+            trash::linux_windows::purge_all(trash::linux_windows::list().unwrap()).unwrap();
+        }
+        ("rm", Some(matches)) => trash::remove_all(matches.values_of("path").unwrap()).unwrap(),
+        _ => {}
+    }
 }
